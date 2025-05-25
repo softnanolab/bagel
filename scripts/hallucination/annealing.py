c3c765de8f4526b1c84adbcaa685fd704f98b557
Jakub Lala <jakublala@gmail.com>
2025-05-25 11:15:14 +0000

    upgrade templates

--- scripts/hallucination/annealing.py
+++ scripts/hallucination/annealing.py
@@ -4,16 +4,25 @@ import bagel as bg
 sequence = np.random.choice(list(bg.constants.aa_dict.keys()), size=100)
 residues = [bg.Residue(name=aa, chain_ID="A", index=i, mutable=True) for i, aa in enumerate(sequence)]
 
+esmfold = bg.folding.ESMFold(use_modal=True)
+
 state = bg.State(
     chains = [bg.Chain(residues)],
-    energy_terms = [bg.energies.PTMEnergy(), bg.energies.OverallPLDDTEnergy(), bg.energies.HydrophobicEnergy()],
-    energy_terms_weights = [1.0, 1.0, 1.0],
+    energy_terms = [bg.energies.PTMEnergy(
+        oracle=esmfold,
+        weight=1.0,
+    ), bg.energies.OverallPLDDTEnergy(
+        oracle=esmfold,
+        weight=1.0,
+    ), bg.energies.HydrophobicEnergy(
+        oracle=esmfold,
+        weight=1.0,
+    )],
     name = "state_A",
     verbose = True
 )
 
 minimizer = bg.minimizer.SimulatedAnnealing(
-    folder=bg.folding.ESMFolder(use_modal=True),
     mutator=bg.mutation.Canonical(),
     initial_temperature = 0.2,
     final_temperature = 0.02,
--- scripts/scaffolds/annealing.py
+++ scripts/scaffolds/annealing.py
@@ -5,23 +5,23 @@ import bagel as bg
 sequence = np.random.choice(list(bg.constants.aa_dict.keys()), size=190)
 residues = [bg.Residue(name=aa, chain_ID='A', index=i, mutable=True) for i, aa in enumerate(sequence)]
 
+esmfold = bg.folding.ESMFold(use_modal=True)
+
 state = bg.State(
     chains=[bg.Chain(residues)],
     energy_terms=[
-        bg.energies.PTMEnergy(),
-        bg.energies.OverallPLDDTEnergy(),
-        bg.energies.HydrophobicEnergy(surface_only=True),
-        bg.energies.RingSymmetryEnergy([residues[i*50 : (i*50)+40] for i in range(4)]),
+        bg.energies.PTMEnergy(oracle=esmfold, weight=1.0),
+        bg.energies.OverallPLDDTEnergy(oracle=esmfold, weight=1.0),
+        bg.energies.HydrophobicEnergy(oracle=esmfold, weight=3.0, surface_only=True),
+        bg.energies.RingSymmetryEnergy(oracle=esmfold, weight=1.0, groups=[residues[i*50 : (i*50)+40] for i in range(4)]),
         # 5 residues on either side of each symmetry group unconstrained to allow for chain flexibility
-        bg.energies.SecondaryStructureEnergy([residues[(i*50)+5 : (i*50)+35] for i in range(4)], 'beta-sheet'),
+        bg.energies.SecondaryStructureEnergy(oracle=esmfold, weight=1.0, groups=[residues[(i*50)+5 : (i*50)+35] for i in range(4)], structure_type='beta-sheet'),
     ],
-    energy_terms_weights=[1.0, 1.0, 3.0, 1.0, 1.0],
     name='state_A',
 )
 
 minimizer = bg.minimizer.SimulatedAnnealing(
-    all_folding_algorithms={'EsmFold': bg.folding.ESMFolder(use_modal=True)},
-    mutation_protocol=bg.mutation.Canonical(),
+    mutator=bg.mutation.Canonical(),
     max_mutations_per_step=1,
     initial_temperature=0.2,
     final_temperature=0.05,
