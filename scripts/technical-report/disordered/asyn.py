8890730c05c2c2a3355ea7459ae0af570d797ca5
Jakub Lála <68380659+jakublala@users.noreply.github.com>
2025-06-11 21:28:55 +0100

    Templates for running optimization from technical report (#56)
    * Added input for selective binder
    
    * Adding script for selective binder
    
    * removed unnecessary file
    
    * all templates from manuscript added
    
    ---------
    
    Co-authored-by: sangioletti <67963282+sangioletti@users.noreply.github.com>

---
+++ scripts/technical-report/disordered/asyn.py
@@ -0,0 +1,153 @@
+import random
+import fire
+import bagel as bg
+import os
+import logging
+import pathlib as pl
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+os.environ["HF_MODEL_DIR"] = os.path.expanduser("~/hugging_face/")
+
+def main(
+    use_modal: bool = False,
+    binder_sequence: str = None,
+    optimization_params: dict = None,
+    output_dir: str = 'data/ASYN-binder'
+):
+    
+    # Check
+    print(f'Whether to use modal: {use_modal}')
+
+    # PART 1: Define the target protein
+    # UniProt P37840
+    target_sequence = "MDVFMKGLSKAKEGVVAAAEKTKQGVAEAAGKTKEGVLYVGSKTKEGVVHGVATVAEKTKEQVTNVGGAVVTGVTAVAQKTVEGAGSIAAATGFVKKDQLGKNEEGAPQEGILEDMPVDPDNEAYEMPSEEGYQDYEPEA"
+    
+    # Now define the mutability of the residues, all immutable in this case since this is the target sequence
+    mutability = [False for _ in range(len(target_sequence))]
+    
+    # Now define the chain
+    residues_target = [
+        bg.Residue(name=aa, chain_ID='ASYN', index=i, mutable=mut)
+        for i, (aa, mut) in enumerate(zip(target_sequence, mutability))
+    ]
+
+    # Now define residues in the hotspot where you want to bind.
+    # Last 98-140 AAs are 'disordered', i.e. targeted
+    residue_ids = range(99, 141)
+
+    residues_hotspot = [residues_target[i] for i in residue_ids]
+    target_chain = bg.Chain(residues=residues_target)
+
+    # For the binder, start with a random sequence of amino acids selecting randomly from the 30 amino acids
+    binder_length = 50
+
+    # Jakub: RESTART!
+    if binder_sequence is None:
+        binder_sequence = ''.join([random.choice(list(bg.constants.aa_dict.keys())) for _ in range(binder_length)])
+    else:
+        assert len(binder_sequence) == binder_length, 'Binder sequence must be of length 30'
+
+
+    # Now define the mutability of the residues, all mutable in this case since this is the design sequence
+    mutability = [True for _ in range(len(binder_sequence))]
+    # Now define the chain
+    residues_binder = [
+        bg.Residue(name=aa, chain_ID='BIND', index=i, mutable=mut)
+        for i, (aa, mut) in enumerate(zip(binder_sequence, mutability))
+    ]
+    binder_chain = bg.Chain(residues=residues_binder)
+
+    # Now define the folding algorithm, run locally not on modal
+    config = {
+        'output_pdb': False,
+        'output_cif': False,
+        'glycine_linker': "",
+        'position_ids_skip': 512,
+    }
+    
+    esmfold = bg.oracles.ESMFold(
+        use_modal=use_modal, config=config
+    )
+
+    # Now define the energy terms to be applied to the chain. In this example, all terms apply to all residues
+    energy_terms = [
+        bg.energies.PTMEnergy(
+            oracle=esmfold,
+            weight=1.0,
+        ),
+        bg.energies.OverallPLDDTEnergy(
+            oracle=esmfold,
+            weight=1.0,
+        ),
+        bg.energies.PLDDTEnergy(
+            oracle=esmfold,
+            residues=residues_binder,
+            weight=4.0
+        ),
+        bg.energies.HydrophobicEnergy(
+            oracle=esmfold,
+            weight=2.0
+            ),
+        bg.energies.PAEEnergy(
+            oracle=esmfold,
+            residues=[residues_hotspot, residues_binder],
+            weight=4.0,
+        ),
+        bg.energies.SeparationEnergy(
+            oracle=esmfold,
+            residues=[residues_hotspot, residues_binder],
+            weight=0.2,
+        ),
+    ]
+
+    state = bg.State(
+        chains=[binder_chain, target_chain],
+        energy_terms=energy_terms,
+        name='state',
+    )
+
+    # Now define the system
+    initial_system = bg.System(
+        states=[state],
+        name='ASYN-binder'
+    )
+
+    # Now define the minimizer
+    mutator = bg.mutation.Canonical()
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    print(f'Current directory: {current_dir}')
+    
+    # Use optimization parameters if provided, otherwise use defaults
+    if optimization_params is None:
+        optimization_params = {
+            'high_temperature': 2.0,
+            'low_temperature': 0.2,
+            'n_steps_high': 50,
+            'n_steps_low': 50,
+            'n_cycles': 200,
+        }
+
+    
+    minimizer = bg.minimizer.SimulatedTempering(
+        mutator=mutator,
+        high_temperature=optimization_params['high_temperature'],
+        low_temperature=optimization_params['low_temperature'],
+        n_steps_high=optimization_params['n_steps_high'],
+        n_steps_low=optimization_params['n_steps_low'],
+        n_cycles=optimization_params['n_cycles'],
+        preserve_best_system_every_n_steps=optimization_params['n_steps_high'] + optimization_params['n_steps_low'],
+        log_frequency=1,
+        log_path=pl.Path(os.path.join(current_dir, output_dir)),
+    )
+
+    # Run optimization and return the best system
+    best_system = minimizer.minimize_system(system=initial_system)
+    return best_system
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
---
+++ scripts/technical-report/mimic_oxidoreductase.py
@@ -0,0 +1,95 @@
+import bagel as bg
+import os
+import numpy as np
+from typing import Any
+import modal
+
+# This script implements the method described in Rajendran et al 2025 to mimic enzymes
+# using the ESM2 embeddings. It uses the Metropolis minimizer to generate a diverse set of structures
+# that maintain the environment of the conserved residues, while mutating the rest of the residues.
+
+def run_generate_mimic() -> Any:
+
+    # Get the value of an environment variable
+    os.environ["HF_MODEL_DIR"] = os.path.expanduser("~/hugging_face/")
+    use_modal = False
+
+    # Check
+    print(f'Whether to use modal: {use_modal}')
+
+    # Define the target enzyme we want to mimic
+    # Here it is (just to make a random example):
+    # >1A2J_1|Chain A|DISULFIDE BOND FORMATION PROTEIN|Escherichia coli (562)
+    # AQYEDGKQYTTLEKPVAGAPQVLEFFSFFCPHCYQFEEVLHISDNVKKKLPEGVKMTKYHVNFMGGDLGKDLTQAWAVAMALGVEDKVTVPLFEGVQKTQTIRSASDIRDVFINAGIKGEEYDAAWNSFVVKSLVAQQEKAAADVQLRGVPAMFVNGKYQLNPQGMDTSNMDVFVQQYADTVKYLSEKK
+    target_sequence = 'AQYEDGKQYTTLEKPVAGAPQVLEFFSFFCPHCYQFEEVLHISDNVKKKLPEGVKMTKYHVNFMGGDLGKDLTQAWAVAMALGVEDKVTVPLFEGVQKTQTIRSASDIRDVFINAGIKGEEYDAAWNSFVVKSLVAQQEKAAADVQLRGVPAMFVNGKYQLNPQGMDTSNMDVFVQQYADTVKYLSEKK'
+
+    # Non-mutable should be CYS-30 and CYS-33, 1-indexed, so 0-index it
+    immutable_list = [30 - 1, 33 - 1]
+    mutable_list = [i for i in range(len(target_sequence)) if i not in immutable_list]
+    mutability = [True if i in mutable_list else False for i in range(len(target_sequence))] 
+    print(f"Length of target sequence: {len(target_sequence)}")
+    print(f"Mutability: {mutability}")
+    print(f"Immutable list: {immutable_list}")
+    print(f"Mutable list: {mutable_list}")
+    
+    # Define a chain providing a list of residues
+    all_residues = [
+        bg.Residue(name=aa, chain_ID='zing', index=i, mutable=mut)
+        for i, (aa, mut) in enumerate(zip(target_sequence, mutability))
+    ]
+    # For use later, define the conserved residues
+    # These are the residues that we want to keep constant and whose environment we want to maintain
+    # In this case, these are the residues that are not mutable
+    conserved_residues = [res for res in all_residues if res.mutable is False]
+    print( f"Number of conserved residues: {len(conserved_residues)}" )
+    
+    # Define the chain object
+    chain = bg.Chain(residues=all_residues)
+
+    # Extract embeddings for the residues that you want to remain constant and whose environment
+    # you want to maintain 
+    # First define the EmbeddingOracle
+    esm2 = bg.oracles.ESM2( use_modal=use_modal, config={'model_name': 'esm2_t33_650M_UR50D'})
+    # Embed the chain to get the embeddings for the residues
+    result = esm2.embed( chains=[chain] )
+    # Extract only the embeddings for the immutable residues
+    immutable = ~np.array( mutability )  # get the immutable residues (those that are not mutable)
+    reference_embeddings = result.embeddings[immutable]  # get the embeddings for the reference (immutable) residues
+
+    # Define the energy terms to be applied to the chain. Apply them to residues, and specify the weight
+    # Following Rajendran et al 2025, we use the EmbeddingsSimilarityEnergy of the conserved residues
+    energy_terms = [
+        bg.energies.EmbeddingsSimilarityEnergy(
+            oracle=esm2,
+            weight=1.0,
+            residues=conserved_residues,  # apply the energy term to the conserved residues
+            reference_embeddings=reference_embeddings,)]
+
+    # Define the state
+    state = bg.State(
+        name='state_A',
+        chains=[chain],
+        energy_terms=energy_terms,
+    )
+
+    # Define the system - it's a collection of states but here there is only one.
+    initial_system = bg.System(states=[state])
+
+    # Define the minimizer
+    # In this case, we want to do MC sampling to generate a diverse set of structures,
+    # see Rajendran et al 2025. So we do standard MC with fixed temperature of 1e-4. 
+    # We are not really minimizing the system, but rather generating a diverse set of structures
+    # at a fixed temperature in this way.
+    minimizer = bg.minimizer.MonteCarloMinimizer(
+        mutator=bg.mutation.Canonical(n_mutations=1), # cannot add/remove residues, only substitutes for different amino acid types
+        temperature=1e-4,  # fixed temperature for the MC sampling
+        n_steps=10000,  # number of steps to run the minimizer
+        log_frequency=1,
+    )
+
+    final_system = minimizer.minimize_system(system=initial_system)
+
+    return final_system
+
+if __name__ == '__main__':
+    run_generate_mimic()
