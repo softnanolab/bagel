f0d22e720a832f187349c10dd29853ef9934ff6f
Stefano Angioletti-Uberti <67963282+sangioletti@users.noreply.github.com>
2025-08-13 12:21:10 +0100

    Improved residue selection in tools

--- src/bagel/tools.py
+++ src/bagel/tools.py
@@ -9,18 +9,28 @@ import copy
 aa_dict_3to1 = {v: k for k, v in bg.constants.aa_dict.items()}
 # Functions to combine different chains / extract them from a .pdb file to make composite chains
 
-def get_atomarray_in_residue_range(atoms: AtomArray, start: int, end: int , chain: str) -> AtomArray:
+def get_atomarray_in_residue_range(atoms: AtomArray, start: int = None, end: int = None, 
+                                   chain: str = None ) -> AtomArray:
     """Extract a range of residues with a specific chain_id from an AtomArray object.
     Input:
     - atoms: AtomArray object containing all atoms
-    - start: starting residue index (inclusive)
-    - end: ending residue index (exclusive)
-    - chain: chain identifier (e.g. 'A', 'B')
+    - start: starting residue index (inclusive). Default to None
+    - end: ending residue index (exclusive). Default to None
+    - chain: chain identifier (e.g. 'A', 'B'). Default to None
     Output: 
     - AtomArray object containing the extracted atoms
     """
-    chain_atoms = copy.deepcopy( atoms[atoms.chain_id == chain] )
-    return chain_atoms[np.logical_and(chain_atoms.res_id >= start, chain_atoms.res_id < end)]
+    if start is None:
+        start = atoms.res_id.min()
+    if end is None:
+        end = atoms.res_id.max() + 1
+
+    if chain is not None:
+        chain_atoms = copy.deepcopy( atoms[atoms.chain_id == chain] )
+    else:
+        chain_atoms = copy.deepcopy( atoms )
+
+    return chain_atoms[np.logical_and(chain_atoms.res_id >= start, chain_atoms.res_id <= end)]
 
 def sequence_from_atomarray(atoms: AtomArray) -> str:
     """Extract the sequence of aminoacids written in 1-Letter convention 
